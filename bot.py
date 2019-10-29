3abc4f86a45d6d79e5f3e6917b2020fbd80401dc
Piotr Wieczorek <piotr.wieczorek98@gmail.com>
2019-10-29 17:42:44 +0100

    1.1

--- bot.py
+++ bot.py
@@ -2,41 +2,28 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import os
+import asyncio
+from random import choice
 
 BOT_TOKEN = 'NTk1NzIxNjQ5NzE5MDgzMDA4.Xba5Yw.kEDMTabHk8DHXzMFdLQLXrDsi7k'
 BOT_PREFIX = 'boi '
+SOUNDS_LOC = "sounds/"
 
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 bot.remove_command("help")
+queue = asyncio.Queue()
+play_next_song = asyncio.Event()
+
 
 @bot.event
 async def on_ready():
     print("Logged in as: " + bot.user.name + "\n")
+    await bot.change_presence(status=discord.Status.idle, activity=discord.Game('boi '))
 
-    if not discord.opus.is_loaded():
-        raise RunTimeError('Opus failed to load')
-
-    await bot.change_presence(status=discord.Status.idle, activity = discord.Game('boi '))
 
 ###############################################################################
 ###############################################################################
-@bot.command(pass_context=True, aliases=['j', 'joi'])
-async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-
-        print(f"The bot has connected to {channel}\n")
-
-    await ctx.send(f"No cześć")
-
-###############################################################################
-###############################################################################
-@bot.command(pass_context=True, aliases=['dc','disconnect'])
+@bot.command(aliases=['dc', 'disconnect'])
 async def leave(ctx):
     channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild=ctx.guild)
@@ -47,102 +34,119 @@ async def leave(ctx):
         await ctx.send(f"To nara")
     else:
         print("Bot was told to leave voice channel, but was not in one")
-        await ctx.send("Nawet mnie tam nie ma dbanie")
+        await ctx.send("Nawet mnie tam nie ma dzbanie")
+
 
 ###############################################################################
 ###############################################################################
-@bot.command(pass_context=True, aliases=['p', 'pla'], description = "nazwa pliku bez mp3")
+@bot.command(aliases=['p', 'pla'])
 async def play(ctx, mp3: str):
-    channel = ctx.message.author.voice.channel
     voice = get(bot.voice_clients, guild=ctx.guild)
-    songLoc = "sounds/" + mp3 + ".mp3"
+    sound_loc = SOUNDS_LOC + mp3 + ".mp3"
 
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-        print(f"The bot has connected to {channel}\n")
+    song_there = os.path.isfile(sound_loc)
+    if not song_there:
+        await ctx.send("Nie ma takiego pliku dzbanie")
 
-    song_there = os.path.isfile(songLoc)
-    try:
-        if not song_there:
-            await ctx.send("Nie ma takiego pliku dzbanie")
-    except PermissionError:
-        await ctx.send("ERROR")
-        return
+    if not voice.is_playing():
+        voice.play(discord.FFmpegPCMAudio(sound_loc))
 
-    voice = get(bot.voice_clients, guild=ctx.guild)
-
-    voice.play(discord.FFmpegPCMAudio(songLoc), after=lambda e: print("Song done!"))
-    #voice.source = discord.PCMVolumeTransformer(voice.source)
-    #voice.source.volume = 0.07
 
 ###############################################################################
 ###############################################################################
-@bot.command(pass_context=True, aliases=['pa', 'pau'], description = "pa, pau - wiadomo")
+@bot.command(aliases=['ran', 'los'])
+async def random(ctx):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    sound_name = choice(os.listdir(SOUNDS_LOC))
+    voice.play(discord.FFmpegPCMAudio(SOUNDS_LOC + sound_name))
+
+
+###############################################################################
+###############################################################################
+@bot.command(aliases=['l', 'sounds'])
+async def list(ctx):
+    sounds_list = ""
+    for entry in os.listdir(SOUNDS_LOC):
+        if os.path.isfile(os.path.join(SOUNDS_LOC, entry)):
+            sounds_list += entry + "\n"
+    await ctx.send(sounds_list)
+
+
+###############################################################################
+###############################################################################
+@bot.command(aliases=['pa', 'pau'])
 async def pause(ctx):
-
     voice = get(bot.voice_clients, guild=ctx.guild)
 
     if voice and voice.is_playing():
-        print("Music paused")
+        await ctx.send(":thumbup:")
         voice.pause()
     else:
-        print("Music not playing failed pause")
-        await ctx.send("Jakbych coś groł, to bych to pauznoł")
+        await ctx.send("Coś się popsuło")
+
 
 ###############################################################################
 ###############################################################################
-@bot.command(pass_context=True, aliases=['r', 'res'], description = "r, res - wiadomo")
+@bot.command(aliases=['r', 'res'])
 async def resume(ctx):
-
     voice = get(bot.voice_clients, guild=ctx.guild)
 
     if voice and voice.is_paused():
+        await ctx.send(":thumbup:")
         voice.resume()
     else:
-        print("Music is not paused")
-        await ctx.send("Dyć już leci")
+        await ctx.send("Coś się popsuło")
+
 
 ###############################################################################
 ###############################################################################
-@bot.command(pass_context=True, aliases=['s', 'sto'], description = "s, sto - wiadomo")
+@bot.command(aliases=['s', 'sto'])
 async def stop(ctx):
-
     voice = get(bot.voice_clients, guild=ctx.guild)
 
     if voice and voice.is_playing():
-        print("Music stopped")
+        await ctx.send(":thumbup:")
         voice.stop()
     else:
-        print("No music playing failed to stop")
-        await ctx.send("Jakbych coś groł, to bych to sztopnoł")
+        await ctx.send("Coś się popsuło")
+
 
 ###############################################################################
 ###############################################################################
-@bot.command(pass_context=True, aliases=['l', 'lis'],description = "l, lista - wypisuje listę dźwięków")
-async def list(ctx):
-    basepath = 'sounds/'
-    soundsList = ""
-    for entry in os.listdir(basepath):
-        if os.path.isfile(os.path.join(basepath, entry)):
-            soundsList += entry + "\n"
-    await ctx.send(soundsList)
+@bot.command()
+async def volume(ctx, value: int):
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+    voice.source.volume = value/100
+    await ctx.send("Zmieniono głośność na {}%".format(value))
+
 
 ###############################################################################
 ###############################################################################
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(colour = discord.Colour.orange())
+    embed = discord.Embed(colour=discord.Colour.orange())
     embed.set_author(name='Help')
-    embed.add_field(name='play', value = 'Działa też p, pla. Nazwa pliku bez.mp3', inline = False)
-    embed.add_field(name='list', value = 'Działa też l, lis. Lista dźwięków', inline = False)
-    embed.add_field(name='join', value = 'Działa też j, joi', inline = False)
-    embed.add_field(name='leave', value = 'Działa też dc, disconnect', inline = False)
-    embed.add_field(name='pause', value = 'Działa też pa, pau', inline = False)
-    embed.add_field(name='stop', value = 'Działa też s, sto', inline = False)
-    embed.add_field(name='resume', value = 'Działa też r, res', inline = False)
-    await ctx.send(embed = embed)
+    embed.add_field(name='play', value='Działa też p, pla. Nazwa pliku bez.mp3', inline=False)
+    embed.add_field(name='list', value='Działa też l, lis. Lista dźwięków', inline=False)
+    embed.add_field(name='random', value='Działa też ran, los. Lista dźwięków', inline=False)
+    embed.add_field(name='volume', value='Zmiana głośności', inline=False)
+    embed.add_field(name='leave', value='Działa też dc, disconnect', inline=False)
+    embed.add_field(name='pause', value='Działa też pa, pau', inline=False)
+    embed.add_field(name='stop', value='Działa też s, sto', inline=False)
+    embed.add_field(name='resume', value='Działa też r, res', inline=False)
+    await ctx.send(embed=embed)
 
+@play.before_invoke
+@random.before_invoke
+@volume.before_invoke
+async def ensure_voice(ctx):
+    if ctx.voice_client is None:
+        if ctx.author.voice:
+            await ctx.author.voice.channel.connect()
+        else:
+            await ctx.send("Nie jesteś połączony.")
+    elif ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
 
 bot.run(BOT_TOKEN)
