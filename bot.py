55cca64b8f671bd9f7adc2d2f78e3c8778636808
Piotr Wieczorek <piotr.wieczorek98@gmail.com>
2019-10-31 13:50:51 +0100

    queue added

--- bot.py
+++ bot.py
@@ -5,23 +5,45 @@ import os
 import asyncio
 from random import choice
 
+# BOT SETTINGS
 BOT_TOKEN = 'NTk1NzIxNjQ5NzE5MDgzMDA4.Xba5Yw.kEDMTabHk8DHXzMFdLQLXrDsi7k'
 BOT_PREFIX = 'boi '
 SOUNDS_LOC = "sounds/"
+TIMEOUT = 120
 
+# SET PREFIX, REMOVE HELP AND LIST COMMAND TO REPLACE IT LATER
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 bot.remove_command("help")
-queue = asyncio.Queue()
-play_next_song = asyncio.Event()
+bot.remove_command("list")
 
+# CREATE QUEUE
+queue = []
 
+# WHEN READY CHANGE STATUS AND CREATE BACKGROUND TASK
 @bot.event
 async def on_ready():
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(BOT_PREFIX))
+    bot.bg_task = bot.loop.create_task(audio_player_task())
     print("Logged in as: " + bot.user.name + "\n")
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game('boi '))
 
+
+# BACKGROUND TASK
+async def audio_player_task():
+    counter = 0
+    while not bot.is_closed():
+        if len(queue) > 0:
+            sound_tuple = queue[0]
+            voice = sound_tuple[0]
+
+            if not voice.is_playing():
+                audio_source = sound_tuple[1]
+                voice.play(discord.FFmpegPCMAudio(audio_source))
+                queue.pop(0)
+        counter += 1
+        await asyncio.sleep(1)
 
 ###############################################################################
+#                                   LEAVE
 ###############################################################################
 @bot.command(aliases=['dc', 'disconnect'])
 async def leave(ctx):
@@ -38,21 +60,23 @@ async def leave(ctx):
 
 
 ###############################################################################
+#                                   PLAY
 ###############################################################################
 @bot.command(aliases=['p', 'pla'])
 async def play(ctx, mp3: str):
     voice = get(bot.voice_clients, guild=ctx.guild)
-    sound_loc = SOUNDS_LOC + mp3 + ".mp3"
 
-    song_there = os.path.isfile(sound_loc)
+    audio_source = SOUNDS_LOC + mp3 + ".mp3"
+
+    song_there = os.path.isfile(audio_source)
     if not song_there:
-        await ctx.send("Nie ma takiego pliku dzbanie")
-
-    if not voice.is_playing():
-        voice.play(discord.FFmpegPCMAudio(sound_loc))
-
+        await ctx.send("File not found")
+    else:
+        sound_tuple = (voice, audio_source)
+        queue.append(sound_tuple)
 
 ###############################################################################
+#                                   RANDOM
 ###############################################################################
 @bot.command(aliases=['ran', 'los'])
 async def random(ctx):
@@ -62,6 +86,7 @@ async def random(ctx):
 
 
 ###############################################################################
+#                                   LIST
 ###############################################################################
 @bot.command(aliases=['l', 'sounds'])
 async def list(ctx):
@@ -73,6 +98,7 @@ async def list(ctx):
 
 
 ###############################################################################
+#                                   PAUSE
 ###############################################################################
 @bot.command(aliases=['pa', 'pau'])
 async def pause(ctx):
@@ -86,6 +112,7 @@ async def pause(ctx):
 
 
 ###############################################################################
+#                                   RESUME
 ###############################################################################
 @bot.command(aliases=['r', 'res'])
 async def resume(ctx):
@@ -99,6 +126,7 @@ async def resume(ctx):
 
 
 ###############################################################################
+#                                   STOP
 ###############################################################################
 @bot.command(aliases=['s', 'sto'])
 async def stop(ctx):
@@ -112,6 +140,7 @@ async def stop(ctx):
 
 
 ###############################################################################
+#                                   VOLUME
 ###############################################################################
 @bot.command()
 async def volume(ctx, value: int):
@@ -122,6 +151,7 @@ async def volume(ctx, value: int):
 
 
 ###############################################################################
+#                                   HELP
 ###############################################################################
 @bot.command()
 async def help(ctx):
@@ -137,6 +167,9 @@ async def help(ctx):
     embed.add_field(name='resume', value='Działa też r, res', inline=False)
     await ctx.send(embed=embed)
 
+###############################################################################
+#                                 ENSURE_VOICE
+###############################################################################
 @play.before_invoke
 @random.before_invoke
 @volume.before_invoke
@@ -146,7 +179,5 @@ async def ensure_voice(ctx):
             await ctx.author.voice.channel.connect()
         else:
             await ctx.send("Nie jesteś połączony.")
-    elif ctx.voice_client.is_playing():
-        ctx.voice_client.stop()
 
 bot.run(BOT_TOKEN)
